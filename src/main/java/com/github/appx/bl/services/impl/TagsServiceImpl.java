d5bb2d87f9eaa063a326b355fcb2bf60a3bd0fc6
Linghui, Luo <llinghui@amazon.com>
2020-11-30 14:30:04 +0100

    add resource leak

--- src/main/java/com/github/appx/bl/services/impl/TagsServiceImpl.java
+++ src/main/java/com/github/appx/bl/services/impl/TagsServiceImpl.java
@@ -8,6 +8,15 @@ import com.github.appx.api.Request;
 import com.github.appx.bl.TagsResponse;
 import com.github.appx.bl.dao.TagsDao;
 import com.github.appx.bl.services.TagsService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.log4j.Logger;
 
 public class TagsServiceImpl implements TagsService {
@@ -40,15 +49,26 @@ public class TagsServiceImpl implements TagsService {
             JsonObject brandTags = parser.parse(tagsResponse.getBrandTags()).getAsJsonObject();
 
             brandTags.entrySet().forEach(entry -> defaultTags.add(entry.getKey(), entry.getValue()));
-
+            writeToFile(brandTags);
             return defaultTags;
         } catch (JsonSyntaxException e) {
             LOG.error(":mergeTags: failed to merge common tags with brand tags, using only common. JsonSyntaxException=" + e.getMessage());
             return getDefaultTags(tagsResponse);
-        }
+        } catch (IOException e) {
+        	 LOG.error(":mergeTags: failed to log brandTags" + e.getMessage());
+             return getDefaultTags(tagsResponse);
+		}
     }
 
-    private JsonObject getDefaultTags(TagsResponse tagsResponse) {
+    private void writeToFile(JsonObject brandTags) throws IOException {
+    	File temp =File.createTempFile("tags", ".log");
+    	OutputStream out = new FileOutputStream(temp);
+        Writer writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+        writer.write(brandTags.toString());
+        writer.close();
+	}
+
+	private JsonObject getDefaultTags(TagsResponse tagsResponse) {
         return new JsonParser().parse(tagsResponse.getDefaultTags()).getAsJsonObject();
     }
 
